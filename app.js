b97d95adc52e1c0ca81c0772c8c87bad9b9894fc
Luiz <luizclaudiol@hotmail.com>
2018-01-27 14:38:14 -0300

    creating server

---
+++ app.js
@@ -0,0 +1,23 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+// Cors Middleware
+app.use(cors());
+
+// Port number
+const port = process.env.PORT || 8080;
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'source/build')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Start server
+app.listen(port, () => {
+    console.log(`\n\nServer started on port ${port}`);
+});
