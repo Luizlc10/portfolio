ef44ca32647a04cd4973923bfa8843a66f9b871b
Luiz <luizclaudiol@hotmail.com>
2018-01-27 15:16:37 -0300

    deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 // Cors Middleware
@@ -17,7 +18,16 @@ app.use(express.static(path.join(__dirname, 'source/build')));
 // Body Parser Middleware
 app.use(bodyParser.json());
 
+// Index route
+// app.get('/', (req, res) => {
+//     res.send('Invalid Endpoint');
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'source/build/index.html'));
+});
+
 // Start server
 app.listen(port, () => {
     console.log(`\n\nServer started on port ${port}`);
-}); 
+});
